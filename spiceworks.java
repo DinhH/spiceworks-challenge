7575c6b50a4bbea2f36385af55b0dd35f7b1e689
Dinh Huynh <dhuynh@dhuynh-13-MacBook-Air.local>
2015-10-13 15:43:35 -0500

    init algorithm

---
+++ spiceworks.java
@@ -0,0 +1,32 @@
+public class Node { 
+	public Node firstChild; 
+	public Node nextSibling;
+}
+
+public int countDescendents (Node node) {
+	//Declaring a linkList for the list of nodes
+	LinkList<Node> list = new LinkList<Node>();
+	int numDescendent;
+	//If node has descendants, push it onto the list.
+	if(node.firstChild != NULL && node != NULL) {
+		list.push(node.firstChild);
+		numDescendent++;	
+	}
+	//If list ist not empty, iterate through it.
+	while(!list.isEmpty()) {
+		//Set current node to first on the list
+		Node b = list.pop();
+		//Checking if the firstChild is valid
+		if(b.firstChild != NULL) {
+			list.push(b.firstChild);
+			numDescendent++;
+		}
+		//Checking if nextSibling is valid
+		if(b.nextSibling != NULL) {
+			list.push(b.nextSibling);
+			numDescendent++;
+		}
+	}
+	return numDescendent;
+}
+
